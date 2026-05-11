0518961b923c2b66c7608e04ded7d9ae1cb7de86
mw <m@markwilson.xyz>
2026-05-11 18:55:57 +1200

    info page explosion time

--- src/lib/easter-egg.ts
+++ src/lib/easter-egg.ts
@@ -5,4 +5,4 @@ export const EASTER_EGG_HOVER_MS = 10_000;
 export const INFO_PORTRAIT_EYES_CLICKS = 21;
 
 /** Info portrait: continuous hover before “words fall under gravity” easter egg. */
-export const INFO_PORTRAIT_GRAVITY_HOVER_MS = 30_000;
+export const INFO_PORTRAIT_GRAVITY_HOVER_MS = 12_345;
